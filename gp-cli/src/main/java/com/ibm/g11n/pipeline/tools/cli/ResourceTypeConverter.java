dcd9a57f4a1f7710926ab9487cf00701a31e668e
Yoshito Umaoka <yoshito_umaoka@us.ibm.com>
2016-12-16 18:11:28 -0500

    Fixed ResourceTypeConverter visibility issue
    ResourceTypeConverter was changed to package private at somepoint. But
    it needs to be public, because jcommander needs to access the class.

--- gp-cli/src/main/java/com/ibm/g11n/pipeline/tools/cli/ResourceTypeConverter.java
+++ gp-cli/src/main/java/com/ibm/g11n/pipeline/tools/cli/ResourceTypeConverter.java
@@ -1,5 +1,5 @@
 /*  
- * Copyright IBM Corp. 2015
+ * Copyright IBM Corp. 2015, 2016
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import com.ibm.g11n.pipeline.resfilter.ResourceType;
  * 
  * @author Yoshito Umaoka
  */
-class ResourceTypeConverter implements IStringConverter<ResourceType>{
+public class ResourceTypeConverter implements IStringConverter<ResourceType>{
 
     @Override
     public ResourceType convert(String type) {
