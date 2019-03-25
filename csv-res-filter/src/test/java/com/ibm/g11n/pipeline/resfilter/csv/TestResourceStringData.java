516f6c3df87f5137282262bed648b9b4fb30e8f9
Parth Gaglani <gaglani@ca.ibm.com>
2019-03-25 14:13:00 -0400

    adding parent and update package

--- csv-res-filter/src/test/java/com/ibm/g11n/pipeline/resfilter/csv/TestResourceStringData.java
+++ csv-res-filter/src/test/java/com/ibm/g11n/pipeline/resfilter/csv/TestResourceStringData.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ibm.g11n.pipeline;
+package com.ibm.g11n.pipeline.resfilter.csv;
 
 class TestResourceStringData {
     String key;
--- csv-res-filter/src/test/java/com/ibm/g11n/pipeline/resfilter/csv/TestUtils.java
+++ csv-res-filter/src/test/java/com/ibm/g11n/pipeline/resfilter/csv/TestUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ibm.g11n.pipeline;
+package com.ibm.g11n.pipeline.resfilter.csv;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
